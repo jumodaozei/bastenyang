566e8f79451b3e9261230ba5eb1be582b609152f
bastenyang <61124947@qq.com>
2019-10-24 10:11:36 +0800

    搭建sku骨架页面

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,4 +1,6 @@
 // pages/detail/detail.js
+import {Spu} from "../../model/spu";
+
 Page({
 
     /**
@@ -9,7 +11,12 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: async function (options) {
+        const pid = options.pid;
+        const spu = await Spu.getDetail(pid);
+        this.setData({
+            spu
+        });
 
     },
 
